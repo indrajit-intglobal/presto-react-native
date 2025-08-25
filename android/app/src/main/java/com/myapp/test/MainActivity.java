7de5f0f5a5c300c2f173ce3f67b1307d5f591035
indrajit-intglobal <indrajit.ghosh@intglobal.com>
2025-08-25 15:39:28 +0530

    first

---
+++ android/app/src/main/java/com/myapp/test/MainActivity.java
@@ -0,0 +1,5 @@
+package com.myapp.test;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myapp.test',
+  appName: 'GymEquipmentApp',
+  webDir: 'dist'
+};
+
+export default config;
